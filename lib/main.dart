815c3bb555ba7b36c400dc085201224ae7f53c95
udit <anmolsrivastava60669@gmail.com>
2024-02-21 13:00:38 +0530

    That is fetch data from internet though flutter

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo ',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
